812852bc686184d4f8e96f75f5b39920317b78b7
Mohammad Wasiq Khan <mohdwasiqkhan123@gmail.com>
2025-09-05 02:24:56 +0530

    Adding first folder L4

---
+++ L4_patterns/square.cpp
@@ -0,0 +1,19 @@
+//Write the code to print the below pattern 
+// 1 2 3 4
+// 1 2 3 4
+// 1 2 3 4
+// 1 2 3 4
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n=4;
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
