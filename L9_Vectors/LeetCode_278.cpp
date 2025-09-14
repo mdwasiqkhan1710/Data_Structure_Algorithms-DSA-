17bbea232bebcad650996eb576d1fffe644eb616
Mohammad Wasiq Khan <mohdwasiqkhan123@gmail.com>
2025-09-15 02:03:16 +0530

    Added LC-278

---
+++ L9_Vectors/LeetCode_278.cpp
@@ -0,0 +1,34 @@
+//First Bad Verison
+
+// You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+// Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+// You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int firstBadVersion(int n) {
+        int st=1, end=n;
+        int ans;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+
+            //isBadVerion is an API fuction that is already declared in question
+            if(isBadVersion(mid)==true){
+                ans = mid;
+                end = mid-1;
+            }else{
+                st=mid+1;
+            }
+    }
+    return ans;
+    }
+
+    int main(){
+        int n=5;
+        cout<<firstBadVersion(n)<<endl;
+        return 0;
+    }
